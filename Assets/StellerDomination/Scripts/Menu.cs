da8d61e9026f46144b1ba83b308753929979e1f3
jonobarel <github.com@jbarel.com>
2022-01-09 09:13:04 +0200

    ship selection
    - added ship classes
    - added menu screen for connection
    - created (not implemented) ship selection screen

---
+++ Assets/StellerDomination/Scripts/Menu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class Menu : MonoBehaviour
+{
+    public NetworkManager networkManager;
+    public InputField address;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartHost()
+    {
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        if (address.text == null || address.text == "")
+        {
+            address.text = "http://127.0.0.1";
+        }
+        else
+        {
+            address.text = "http://" + address.text;
+        }
+        Debug.Log("Starting client at address " + address.text);
+        NetworkManager.singleton.StartClient(new System.Uri(address.text));
+    }
+}
